76479bbdbba061540cad155d1447b1659c80fc86
m.fritsche <m.fritsche@security-robotics.de>
2024-03-14 10:01:43 +0100

    Enable Nerd Font and add autopairs and neotree plugins

---
+++ lua/custom/plugins/neotree.lua
@@ -0,0 +1,14 @@
+-- File: ~/.config/nvim/lua/custom/plugins/neotree.lua
+
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  config = function ()
+    require('neo-tree').setup {}
+  end,
+}
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -48,6 +48,10 @@ require('lazy').setup({
 
   require 'kickstart/plugins/treesitter',
 
+  require 'custom/plugins/autopairs',
+
+  require 'custom/plugins/neotree',
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- put them in the right spots if you want.
