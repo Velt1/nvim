492a2a9e1b005c282f7414132174ff311378a861
m.fritsche <m.fritsche@security-robotics.de>
2024-03-14 11:13:29 +0100

    Add lazy plugins for copilot, todo, and onedark_theme

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -54,6 +54,10 @@ require('lazy').setup({
   
   require 'custom/plugins/copilot',
 
+  require 'custom/plugins/todo',
+
+  require 'custom/plugins/onedark_theme',
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- put them in the right spots if you want.
